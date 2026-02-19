e4483115b5022fdef9bedff5ba39710a72497ed9
EricKugel <erickugel713@gmail.com>
2026-02-19 00:09:28 -0600

    Final edits

--- src/vlcr/main.py
+++ src/vlcr/main.py
@@ -19,21 +19,26 @@ def main():
     playlist_file = open(filepath, "w")
     playlist_file.write("#EXTM3U\n")
 
+    # This creates the actual playlist entry string
     def create_entry(path):
         try:
             metadata = audio_metadata.load(path)
+            # Duration in seconds
             duration = math.ceil(metadata["streaminfo"]["duration"])
+            # VLC expects uris
             filepath = path.as_uri()
             try:
                 title = metadata["tags"]["title"][0]
             except:
                 # This song doesn't have a title
                 title = str(path)
+            # m3u standard
             return f"#EXTINF:{duration},{title}\n{filepath}\n"
         except audio_metadata.exceptions.UnsupportedFormat:
             # This is not an audio file
             return ""
 
+    # Recursively finds all files.
     def recurse_dir(path):
         if path.is_file():
             entry = create_entry(path)
@@ -41,12 +46,11 @@ def main():
         else:
             for child in path.iterdir():
                 recurse_dir(child)
-
-    # Recursively get all files
     recurse_dir(root)
 
     playlist_file.close()
 
+    # Starts vlc with the playlist, decoupled from the process.
     subprocess.Popen(
         ["vlc", filepath.as_posix()],
         start_new_session=True,
