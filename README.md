20c8a959c2eab5ec2219f334d83e3eebb4666f54
EricKugel <erickugel713@gmail.com>
2026-02-19 00:06:40 -0600

    Finished vlcr

---
+++ README.md
@@ -0,0 +1,19 @@
+# VLC Recursive
+
+When you open a folder in VLC and try to shuffle all contents recursively, VLC picks one subfolder and plays all files from that, then repeats. This isn't ideal. This is a script to create a playlist file including ALL files in a directory, recursively.
+
+This is built with a python package to make it easy to get the command installed.
+
+### Installation
+
+```
+pip install .
+```
+
+### Usage
+
+```
+vlcr [PATH]
+```
+
+Ignore any AudioMetadataWarning warnings.
---
+++ src/vlcr/main.py
@@ -0,0 +1,55 @@
+import sys
+from pathlib import Path
+import random
+import audio_metadata
+import math
+import tempfile
+import subprocess
+
+def main():
+    # Ensure user typed in a path
+    if len(sys.argv) == 1:
+        print(f"Usage: {sys.argv[0]} DIR")
+        quit()
+
+    root = Path(sys.argv[1])
+
+    # Can't delete tmp files, so add randomness
+    filepath = Path.joinpath(Path(tempfile.gettempdir()), f"vlcr-playlist-{random.randint(0, 999_999)}.m3u")
+    playlist_file = open(filepath, "w")
+    playlist_file.write("#EXTM3U\n")
+
+    def create_entry(path):
+        try:
+            metadata = audio_metadata.load(path)
+            duration = math.ceil(metadata["streaminfo"]["duration"])
+            filepath = path.as_uri()
+            try:
+                title = metadata["tags"]["title"][0]
+            except:
+                # This song doesn't have a title
+                title = str(path)
+            return f"#EXTINF:{duration},{title}\n{filepath}\n"
+        except audio_metadata.exceptions.UnsupportedFormat:
+            # This is not an audio file
+            return ""
+
+    def recurse_dir(path):
+        if path.is_file():
+            entry = create_entry(path)
+            playlist_file.write(entry)
+        else:
+            for child in path.iterdir():
+                recurse_dir(child)
+
+    # Recursively get all files
+    recurse_dir(root)
+
+    playlist_file.close()
+
+    subprocess.Popen(
+        ["vlc", filepath.as_posix()],
+        start_new_session=True,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
